8d8eac6fc887f258974d51ff9cbaeb9cbc0bce5f
Mark Peters <mapeters101@earthlink.net>
2025-05-13 21:06:52 -0400

    Work on lesson072 to 4:50.

---
+++ lesson072.py
@@ -0,0 +1,141 @@
+
+class Color:
+    RED = 1
+    BLACK = 2
+
+
+class Node:
+
+    def __init__(self, data, parent=None, color=Color.RED):
+        self.data = data
+        self.left_node = None
+        self.right_node = None
+        self.parent = parent
+        self.color = color
+
+
+class RedBlackTree:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root is None:
+            self.root = Node(data)
+        else:
+            self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+
+        if data < node.data:
+            if node.left_node:
+                self.insert_node(data, node.left_node)
+            else:
+                node.left_node = Node(data, node)
+                self.settle_violation(node.left_node)
+        else:
+            if node.right_node:
+                self.insert_node(data, node.right_node)
+            else:
+                node.right_node = Node(data, node)
+                self.settle_violation(node.right_node)
+
+    def settle_violation(self, node):
+
+        while node != self.root and self.is_red(node) and self.is_red(node.parent):
+            parent_node = node.parent
+            grand_parent_node = parent_node.parent
+
+            # parent is a left child of it's parent (so the grandparent)
+            if parent_node == grand_parent_node.left_node:
+                uncle = grand_parent_node.right_node
+
+                # case 1.) and case 4.) RECOLORING
+                if uncle and self.is_red(uncle):
+                    print("Re-coloring node %s to RED" % grand_parent_node.data)
+                    grand_parent_node.color = Color.RED
+                    print("Re-coloring node %s to BLACK" % parent_node.data)
+                    parent_node.color = Color.BLACK
+                    uncle.color = Color.BLACK
+                    node = grand_parent_node
+
+
+
+
+
+
+
+
+
+
+    def is_red(self, node):
+        if node is None:
+            return False
+
+        return node.color == Color.RED
+
+
+
+    def traverse(self):
+        if self.root:
+            self.in_order_traversal()
+
+    def in_order_traversal(self, node):
+        if node.left_node:
+            self.in_order_traversal(node.left_node)
+
+        print(node.data)
+
+        if node.right_node:
+            self.in_order_traversal(node.right_node)
+
+    def rotate_right(self, node):
+        print("Rotating to the right on node ", node.data)
+
+        temp_left_node = node.left_node
+        t = temp_left_node.right_node
+
+        temp_left_node.right_node = node
+        node.left_node = t
+
+        if t is not None:
+            t.parent = node
+
+        temp_parent = node.parent
+        node.parent = temp_left_node
+        temp_left_node.parent = temp_parent
+
+        if temp_left_node.parent is not None and temp_left_node.parent.left_node == node:
+            temp_left_node.parent.left_node = temp_left_node
+
+        if temp_left_node.parent is not None and temp_left_node.parent.right_node == node:
+            temp_left_node.parent.right_node = temp_left_node
+
+        if node == self.root:
+            self.root = temp_left_node
+
+    def rotate_left(self, node):
+        print("Rotating to the left on node ", node.data)
+
+        temp_right_node = node.left_node
+        t = temp_right_node.left_node
+
+        temp_right_node.left_node = node
+        node.right_node = t
+
+        if t is not None:
+            t.parent = node
+
+        temp_parent = node.parent
+        node.parent = temp_right_node
+        temp_right_node.parent = temp_parent
+
+        if temp_right_node.parent is not None and temp_right_node.parent.left_node == node:
+            temp_right_node.parent.left_node = temp_right_node
+
+        if temp_right_node.parent is not None and temp_right_node.parent.right_node == node:
+            temp_right_node.parent.right_node = temp_right_node
+
+        if node == self.root:
+            self.root = temp_right_node
+
